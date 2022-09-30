7dc3ac0948dd4bc6067c8642487a563bd560f9a7
Aaryan Shroff <aaryanshroff@Aaryans-MacBook-Pro.local>
2022-09-30 19:33:51 -0400

    Add DynamoDB connection

---
+++ db/rentals.py
@@ -0,0 +1,39 @@
+import boto3
+from boto3 import dynamodb
+from boto3.dynamodb.conditions import Key
+from botocore.exceptions import ClientError
+
+
+class Rentals:
+    """Encapsulates an Amazon DynamoDB table of rentals data."""
+
+    dyn_resource = boto3.resource("dynamodb")
+    table = dyn_resource.Table("ScraperHistory")
+
+    @classmethod
+    def query(cls, city):
+        """
+        Queries for rentals in the specified city.
+
+        :param city: The city to query for.
+        :return: A list of rental items.
+        """
+        try:
+            response = cls.table.scan()
+        except ClientError as e:
+            logger.error(
+                f"Couldn't query for cities in {city}. Here's why: {e.response['Error']['Code']}: {e.response['Error']['Message']}"
+            )
+            raise
+        else:
+            return response["Items"]
+
+
+if __name__ == "__main__":
+    import logging
+    import sys
+
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+    logger = logging.getLogger(__name__)
+
+    logger.info(Rentals.query("Toronto"))
